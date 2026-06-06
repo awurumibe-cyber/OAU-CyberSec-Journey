e0a4e6b96cae2a3f7fbc5cf471c84883a484b812
Awurumibe Chukwuebuka <awurumibechukwuebuka@gmail.com>
2026-06-06 11:08:26 +0100

    Add hello_oau.py - Script 1 variables lab

---
+++ hello_oau.py
@@ -0,0 +1,5 @@
+name = "Awurumibe Chukwuebuka Kingsley"
+matric = "YOUR_OAU_MATRIC_HERE"  # Change this to your actual matric number!
+goal = "Future Cloud Security Architect"
+
+print(f"{name} | {matric} | {goal}")
